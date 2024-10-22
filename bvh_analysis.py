4f93dc4b70cf20f32438bc4008071d855823a46b
AliParky <aparkinson670@gmail.com>
2024-10-22 20:11:00 +0800

    Add Z Rotation to plot for RIGHT_UPPER_LEG rotations

--- bvh_analysis.py
+++ bvh_analysis.py
@@ -59,6 +59,7 @@ lower_x_rot, lower_y_rot, lower_z_rot = extract_joint_rotations(bvh_data, 'RIGHT
 # Plot the rotations for RIGHT_UPPER_LEG
 plt.plot(upper_x_rot, label='X Rotation')
 plt.plot(upper_y_rot, label='Y Rotation')
+plt.plot(upper_z_rot, label='Z Rotation')
 plt.xlabel('Frame')
 plt.ylabel('Rotation (degrees)')
 plt.title('RIGHT_UPPER_LEG Rotations')
