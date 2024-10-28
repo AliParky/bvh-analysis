1a0ef584340e843d8e58310630ad965bc5d95fc3
AliParky <aparkinson670@gmail.com>
2024-10-28 20:36:02 +0800

    Convert frames to time for plot x-axis

--- bvh_analysis.py
+++ bvh_analysis.py
@@ -61,6 +61,9 @@ def calculate_joint_angle(upper_x, upper_y, upper_z, lower_x, lower_y, lower_z):
 upper_x_rot, upper_y_rot, upper_z_rot = extract_joint_rotations(bvh_data, 'RIGHT_UPPER_LEG')
 lower_x_rot, lower_y_rot, lower_z_rot = extract_joint_rotations(bvh_data, 'RIGHT_LOWER_LEG')
 
+# Convert frames to time (seconds)
+time_axis = [frame * frame_time for frame in range(num_frames)]
+
 # Plot the rotations for RIGHT_UPPER_LEG
 plt.figure(figsize=(10, 5))
 plt.plot(upper_x_rot, label='X Rotation')
